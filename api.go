5851b9bff69376407332b2411c7f78d2c54124b0
Raditss <raditya.naufal.a@gmail.com>
2023-05-05 22:54:43 +0700

    support multiple query

--- api.go
+++ api.go
@@ -34,8 +34,8 @@ type ResultResponse struct {
 func FuncHandler(w http.ResponseWriter, r *http.Request) {
 	// Parse request body
 	var requestBody struct {
-		Expr 	string `json:"expr"`
-		Alg 	string `json:"alg"`
+		Expr string `json:"expr"`
+		Alg  string `json:"alg"`
 	}
 
 	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
@@ -44,47 +44,46 @@ func FuncHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	input := requestBody.Expr
-	parts:=strings.Split(input, "?")
+	parts := strings.Split(input, "?")
 	Alg := requestBody.Alg
-	input = strings.ToLower(parts[0])
-	inputType := defineType(input)
-	result:= ""
-	fmt.Print(input)
-	var numres float64
 	var err error
-	if input == "" {
-		// Return error if input is missing
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(ErrorResponse{"Missing input expression"})
-		return
-	}
-
-	// Evaluate input expression
-	switch inputType {
-	case "calculator":
-		numres,err = calculate(input)
-		result = strconv.FormatFloat(numres, 'f', 2, 64)
-	case "date":
-		// parse so only the date is lef
-		result = getDayOfWeek(input)
-	case "textQuestion":
-		qtype := defineQuestionType(input)
-		switch{
-		case qtype == "add":
-			result= addQuestionToDB(input)
-		case qtype == "del":
-			result = delQuestion(input)
-		case qtype == "question":
-			if Alg == "KMP"{
-				result = getAnswerKMP(input)
-			}else if Alg == "BM"{
-				result = getAnswerBM(input)
+	result := ""
+	var resultarr = make([]string, len(parts))
+	for i := 0; i < len(parts)-1; i++ {
+		input = strings.ToLower(parts[i])
+		inputType := defineType(input)
+		var numres float64
+		fmt.Println(inputType)
+		switch inputType {
+		case "calculator":
+			numres, err = calculate(input)
+			resultarr[i] = strconv.FormatFloat(numres, 'f', 2, 64)
+			fmt.Println(resultarr[i])
+		case "date":
+			resultarr[i] = getDayOfWeek(input)
+			fmt.Println(resultarr[i])
+		case "textQuestion":
+			qtype := defineQuestionType(input)
+			switch {
+			case qtype == "add":
+				resultarr[i] = addQuestionToDB(input)
+				fmt.Println(resultarr[i])
+			case qtype == "del":
+				resultarr[i] = delQuestion(input)
+				fmt.Println(resultarr[i])
+			case qtype == "question":
+				if Alg == "KMP" {
+					resultarr[i] = getAnswerKMP(input)
+					fmt.Println(resultarr[i])
+				} else if Alg == "BM" {
+					resultarr[i] = getAnswerBM(input)
+					fmt.Println(resultarr[i])
+				}
+			default:
+				resultarr[i] = ""
 			}
-		default:
-			result = "unknown"
 		}
 	}
-	// push to conv db
 
 	if err != nil {
 		// Return error if input expression is invalid
@@ -92,13 +91,13 @@ func FuncHandler(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(ErrorResponse{err.Error()})
 		return
 	}
+	result= strings.Join(resultarr, " ; ")
 
 	// Return successful result
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(ResultResponse{result})
 
 }
-
 
 func defineType(input string) string {
 	// define what type is the input
@@ -130,8 +129,7 @@ func (s *APIServer) Start() {
 
 	// Add handler for arithmetic expressions
 	router.HandleFunc("/api/question", FuncHandler).Methods("POST")
-	router.HandleFunc("/api/upconv",upConvHandler).Methods("POST")
-	router.HandleFunc("/api/getconv",convHandler).Methods("GET")
+
 
 	// Add CORS middleware to router
 	handler := corsMiddleware(router)
@@ -141,15 +139,15 @@ func (s *APIServer) Start() {
 }
 
 func corsMiddleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-        w.Header().Set("Access-Control-Max-Age", "86400") // 24 hours
-        if r.Method == "OPTIONS" {
-            w.WriteHeader(http.StatusOK)
-            return
-        }
-        next.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Max-Age", "86400") // 24 hours
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
